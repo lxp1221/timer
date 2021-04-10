520113bb77724fd6dfa193b844ea8e5c13e61a27
lxp1221 <78539257+lxp1221@users.noreply.github.com>
2021-04-11 00:35:04 +0800

    first submit

---
+++ js/index.js
@@ -0,0 +1,99 @@
+var timeD = new Vue({
+    el: ".container",
+    data: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        hostr: "00",
+        mistr: "00",
+        sestr: "00",
+        setTime: "00:00:00",
+        seId: 0
+    },
+    mounted: function () {
+        this.inttim();
+        var that = this;
+        this.seId = setInterval(function(){
+            var time = new Date();
+            that.hours = parseInt(time.getHours());
+            that.minutes = parseInt(time.getMinutes());
+            that.seconds = parseInt(time.getSeconds());
+            that.hostr = that.hours < 10 ? '0' + that.hours : '' + that.hours;
+            that.sestr = that.seconds < 10 ? '0' + that.seconds : '' + that.seconds;
+            that.mistr = that.minutes < 10 ? '0' + that.minutes : '' + that.minutes;
+        },1000);
+    },
+    methods: {
+        gettime: function () {
+            this.timing()
+        },
+        refresh: function () {
+            clearInterval(this.seId);
+            this.setTimer();
+        },
+        setT: function () {
+            clearInterval(this.seId);
+            this.setTimer();
+        },
+        ddT:function(){
+            clearInterval(this.seId);
+            var that = this;
+            this.seId = setInterval(function(){
+                var time = new Date();
+                that.hours = parseInt(time.getHours());
+                that.minutes = parseInt(time.getMinutes());
+                that.seconds = parseInt(time.getSeconds());
+                that.hostr = that.hours < 10 ? '0' + that.hours : '' + that.hours;
+                that.sestr = that.seconds < 10 ? '0' + that.seconds : '' + that.seconds;
+                that.mistr = that.minutes < 10 ? '0' + that.minutes : '' + that.minutes;
+            },1000);
+        },
+        timing() {
+            var that = this;
+            this.seId = setInterval(function () {
+                if (that.hours !== 0 && that.minutes === 0 && that.seconds === 0) {
+                    that.minutes = 59;
+                    that.seconds = 59;
+                    that.hours -= 1;
+                } else if (that.minutes !== 0 && that.seconds === 0) {
+                    that.minutes -= 1;
+                    that.seconds = 59;
+                } else if (that.hours === 0 && that.minutes === 0 && that.seconds === 0) {
+                    that.minutes = 0;
+                    that.seconds = 0;
+                    clearInterval(that.seId);
+                    that.play();
+                } else {
+                    that.seconds -= 1;
+                }
+                var ho = that.hours;
+                var se = that.seconds;
+                var mi = that.minutes;
+                that.hostr = ho < 10 ? '0' + ho : '' + ho;
+                that.sestr = se < 10 ? '0' + se : '' + se;
+                that.mistr = mi < 10 ? '0' + mi : '' + mi;
+            }, 1000);
+        },
+        setTimer() {
+            var time = this.setTime.split(":");
+            this.hours = Number(time[0]);
+            this.minutes = Number(time[1]);
+            this.seconds = Number(time[2]);
+            this.hostr = this.hours < 10 ? '0' + this.hours : '' + this.hours;
+            this.sestr = this.seconds < 10 ? '0' + this.seconds : '' + this.seconds;
+            this.mistr = this.minutes < 10 ? '0' + this.minutes : '' + this.minutes;
+        },
+        inttim() {
+            this.setTime = "00:00:00"
+        },
+        play(){
+            const audio = document.getElementById("au");
+            audio.play();
+        },
+        pause(){
+            const audio = document.getElementById("au");
+            audio.pause();
+        }
+    }
+})
+
