9e86efaddf45a1b75f148291bab7ed55872e688a
lxp1221 <78539257+lxp1221@users.noreply.github.com>
2021-04-11 12:02:31 +0800

    禁止复制

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ var timeD = new Vue({
         seId: 0
     },
     mounted: function () {
-        this.disableInfo();
         this.inttim();
         var that = this;
         this.seId = setInterval(function(){
@@ -94,44 +93,7 @@ var timeD = new Vue({
         pause(){
             const audio = document.getElementById("au");
             audio.pause();
-        },
-        disableInfo() {  
-
-                document.onkeydown = function() {  
-            
-                    var e = window.event || arguments[0];  
-            
-                    //屏蔽F12  
-            
-                    if(e.keyCode == 123) {  
-            
-                        return false;  
-            
-                        //屏蔽Ctrl+Shift+I  
-            
-                    } else if((e.ctrlKey) && (e.shiftKey) && (e.keyCode == 73)) {  
-            
-                        return false;  
-            
-                        //屏蔽Shift+F10  
-            
-                    } else if((e.shiftKey) && (e.keyCode == 121)){  
-            
-                        return false;  
-            
-                    }  
-            
-                };  
-            
-                //屏蔽右键单击  
-            
-                document.oncontextmenu = function() {  
-            
-                    return false;  
-            
-                }  
-            
-            }
+        }
     }
 })
 
